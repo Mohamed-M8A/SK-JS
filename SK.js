e0967e6b9461b0ed4551aefef2570aedcc8e419c
Mohamed-M8A <m7md20051968@gmail.com>
2025-08-26 13:39:14 +0300

    Update SK.js

--- SK.js
+++ SK.js
@@ -338,6 +338,36 @@ if (shippingTime) {
   });
 
   // ==============================
+  // ✅ إضافة نجوم التقييم
+  // ==============================
+
+function renderStarsFromValue() {
+  let rating = parseFloat(document.getElementById("ratingValue").textContent);
+
+  let fullStars = Math.floor(rating);          
+  let hasHalf = (rating % 1 !== 0) ? 1 : 0;    
+  let emptyStars = 5 - fullStars - hasHalf;   
+
+  let starsHTML = "";
+
+  for (let i = 0; i < fullStars; i++) {
+    starsHTML += `<span class="star">★</span>`; 
+  }
+
+  if (hasHalf) {
+    starsHTML += `<span class="star half">★</span>`; 
+  }
+
+  for (let i = 0; i < emptyStars; i++) {
+    starsHTML += `<span class="star empty">★</span>`; 
+  }
+
+  document.getElementById("stars").innerHTML = starsHTML;
+}
+
+renderStarsFromValue();
+
+  // ==============================
   // ✅ التعامل مع معلومات المنتج
   // ==============================
 
@@ -665,4 +695,5 @@ el.style.top = position.top + window.pageYOffset + tooltip.caretY - 40 + 'px';
 
   // ==============================
   // ✅ نهاية الإسكربت
+
   // ==============================
