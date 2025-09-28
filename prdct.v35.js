e8cac2d3d44dcafc57c3a133d7bdb9f67779f26d
Mohamed Adel <m7md20051968@gmail.com>
2025-09-28 04:43:29 +0300

    Update prdct.v35.js

--- prdct.v35.js
+++ prdct.v35.js
@@ -1,36 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // ==============================
-  // ✅ إضافة نجوم التقييم
-  // ==============================
-
-function renderStarsFromValue() {
-  let rating = parseFloat(document.getElementById("ratingValue").textContent);
-
-  let fullStars = Math.floor(rating);          
-  let hasHalf = (rating % 1 !== 0) ? 1 : 0;    
-  let emptyStars = 5 - fullStars - hasHalf;   
-
-  let starsHTML = "";
-
-  for (let i = 0; i < fullStars; i++) {
-    starsHTML += `<span class="star">★</span>`; 
-  }
-
-  if (hasHalf) {
-    starsHTML += `<span class="star half">★</span>`; 
-  }
-
-  for (let i = 0; i < emptyStars; i++) {
-    starsHTML += `<span class="star empty">★</span>`; 
-  }
-
-  document.getElementById("stars").innerHTML = starsHTML;
-}
-
-renderStarsFromValue();
-
-  // ==============================
   // ✅ التبويبات الذكية
   // ==============================
 
@@ -569,6 +539,7 @@ el.style.top = position.top + window.pageYOffset + tooltip.caretY - 40 + 'px';
   // ==============================
   // ✅ نهاية الإسكربت
   // ==============================
+
 
 
 
