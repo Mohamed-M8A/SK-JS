717ff67a60df77b508fc3c9e5e07c70a2de21450
Mohamed Adel <m7md20051968@gmail.com>
2025-09-29 23:52:34 +0300

    Update and rename prdct.ui.js to prdct.ui.v1.js

---
+++ prdct.ui.v1.js
@@ -0,0 +1,27 @@
+// ==============================
+// ✅ إضافة نجوم التقييم
+// ==============================
+
+function renderStarsFromValue() {
+  let rating = parseFloat(document.getElementById("ratingValue").textContent);
+
+  let fullStars = Math.floor(rating);          
+  let hasHalf = (rating % 1 !== 0) ? 1 : 0;    
+  let emptyStars = 5 - fullStars - hasHalf;   
+
+  let starsHTML = "";
+
+  for (let i = 0; i < fullStars; i++) {
+    starsHTML += `<span class="star">★</span>`; 
+  }
+
+  if (hasHalf) {
+    starsHTML += `<span class="star half">★</span>`; 
+  }
+
+  for (let i = 0; i < emptyStars; i++) {
+    starsHTML += `<span class="star empty">★</span>`; 
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+
