b49df980246001e562d1fc3c7c0dced01e20fd3e
Mohamed Adel <m7md20051968@gmail.com>
2025-12-13 19:07:26 +0200

    Delete toggleBox.js

--- toggleBox.js
+++
@@ -1,5 +0,0 @@
-// toggleBox.js
-export function toggleBox() {
-    const box = document.getElementById('box');
-    box.style.display = box.style.display === 'none' || box.style.display === '' ? 'block' : 'none';
-}
