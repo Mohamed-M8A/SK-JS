15dbdca09c0196fbaa5930ce82b528d6cb128586
Mohamed Adel <m7md20051968@gmail.com>
2025-12-13 19:07:17 +0200

    Delete textUpdater.js

--- textUpdater.js
+++
@@ -1,7 +0,0 @@
-// textUpdater.js
-export function updateText() {
-    const textElement = document.getElementById('text');
-    const messages = ["Hello World!", "Modular JS!", "Import Works!", "CDN Magic!", "JS is Fun!"];
-    const randomMsg = messages[Math.floor(Math.random() * messages.length)];
-    textElement.textContent = randomMsg;
-}
