92ac9f69585bd81eecad60bd8f5bd0abf460ab28
Mohamed Adel <m7md20051968@gmail.com>
2025-12-13 19:06:51 +0200

    Delete colorChanger.js

--- colorChanger.js
+++
@@ -1,6 +0,0 @@
-// colorChanger.js
-export function changeBackgroundColor() {
-    const colors = ['#ffadad','#ffd6a5','#fdffb6','#caffbf','#9bf6ff','#a0c4ff','#bdb2ff'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-}
